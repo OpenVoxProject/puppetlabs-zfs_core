f21a99a6cb6ae9622d3da80df4dbaf22bf23b111
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2022-11-11 04:50:53 -0600

    fix raid_parity

--- lib/puppet/type/zpool.rb
+++ lib/puppet/type/zpool.rb
@@ -115,7 +115,7 @@ module Puppet
       isnamevar
     end
 
-    newparam(:raid_parity) do
+    newproperty(:raid_parity) do
       desc 'Determines parity when using the `raidz` parameter.'
     end
 
